5e2273dfbcb3841ad8b5eb723357d2bebc1dbb28
dylanjackman <dylan.jackman@my.jcu.edu.au>
2019-08-23 15:35:36 +1000

    fixed for loop shop calculator - added a discount for 10% and created a while loop for if an invalid amount of items is input Completed exceptions and finished the password checker temperatures/broken_score from practical 1 now include functions Warmup - Tested listing properties and learned how to exclude from list or add onto them

---
+++ Prac_04/warmup.py
@@ -0,0 +1,10 @@
+numbers = ["ten", 1, 3, 1, 5, 9, 1]
+"""
+numbers[0] prints off the first number in list. 3 in this case
+-1 goes backwards in list
+3 prints off the 4th number
+Don't know for rest
+"""
+print(numbers[2:])
+print(9 in numbers)
+print("ten" in numbers)
