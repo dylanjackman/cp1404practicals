fc4c475c57267a33d5591b033ac797451bfaa87c
dylanjackman <dylan.jackman@my.jcu.edu.au>
2019-09-18 12:00:17 +1000

    guitar_test can now decide whether or not is vintage and has if statement will print the age

--- Prac_06/guitar_test.py
+++ Prac_06/guitar_test.py
@@ -1,10 +1,12 @@
-from Prac_06.guitars import Guitar
+from Prac_06.guitar import Guitar
 
 
 def main():
     my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    print(my_guitar)
-    print(my_guitar.get_age())
-    print(my_guitar.is_vintage())
+    if my_guitar.is_vintage():
+        print(my_guitar, "\nThis guitar is {} years old which makes this a vintage guitar".format(my_guitar.get_age()))
+    else:
+        print(my_guitar, "\nThis guitar is {} years old which doesn't make it a vintage.... yet.".format(my_guitar.get_age()))
+
 
 main()
