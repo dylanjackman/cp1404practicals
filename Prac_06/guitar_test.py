d69695d73e68ffc5599cda28b345eff79740dacd
dylanjackman <dylan.jackman@my.jcu.edu.au>
2019-09-18 13:15:39 +1000

    Allowed for a list of guitars to be made and print which guitars are vintage and which aren't

--- Prac_06/guitar_test.py
+++ Prac_06/guitar_test.py
@@ -6,7 +6,8 @@ def main():
     if my_guitar.is_vintage():
         print(my_guitar, "\nThis guitar is {} years old which makes this a vintage guitar".format(my_guitar.get_age()))
     else:
-        print(my_guitar, "\nThis guitar is {} years old which doesn't make it a vintage.... yet.".format(my_guitar.get_age()))
+        print(my_guitar, "\nThis guitar is {} years old which doesn't make it a vintage.... yet.".format(my_guitar.
+                                                                                                         get_age()))
 
 
 main()
--- Prac_06/guitars.py
+++ Prac_06/guitars.py
@@ -1,21 +1,32 @@
-class Guitar:
+from Prac_06.guitar import Guitar
 
-    def __init__(self, name="", year=0, cost=0.0):
-        self.name = name
-        self.year = year
-        self.cost = cost
 
-    def __str__(self):
-        return "Guitar: {}, Year of Make({}), worth ${}".format(self.name, self.year, self.cost)
+def main():
+    list_of_guitars = []
 
-    def get_age(self):
-        return 2019 - self.year
-        # age -= self.year
-        # if age < 50:
-        #     return "The guitar is {} years old and is not vintage".format(age)
-        # else:
-        #     return "The guitar is {} years old and is a vintage".format(age)
+    print("My Guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        list_of_guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.\n")
+        name = input("Name: ")
 
-    def is_vintage(self):
-        return self.get_age() >= 50
+    # list_of_guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # list_of_guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
 
+    if list_of_guitars:
+        print("\nThese are mine, you are not allowed to touch. But you may look")
+        for i, guitar in enumerate(list_of_guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {}: {} ({}), worth ${} {}".format(i, guitar.name, guitar.year, guitar.cost,
+                                                            vintage_string))
+    else:
+        print("Why you no have guitar, go go go and get one!")
+
+
+main()
