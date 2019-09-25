1e369644a4b09973b028a35bb65fb545b3373a1a
jc506175 <dylan.jackman@my.jcu.edu.au>
2019-09-25 13:03:12 +1000

    Aligned Code properly

--- Prac_06/programming_language.py
+++ Prac_06/programming_language.py
@@ -8,8 +8,7 @@ class ProgrammingLanguage:
 
     def __str__(self):
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
-                                                                       self.year)
+                                                                           self.year)
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
-
