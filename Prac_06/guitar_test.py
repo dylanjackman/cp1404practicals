5fc28899d877e8ed35f5075adc81aaa4aebd11a1
jc506175 <dylan.jackman@my.jcu.edu.au>
2019-09-25 13:01:41 +1000

    Reformatted code to properly test output code

--- Prac_06/guitar_test.py
+++ Prac_06/guitar_test.py
@@ -3,11 +3,11 @@ from Prac_06.guitar import Guitar
 
 def main():
     my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    if my_guitar.is_vintage():
-        print(my_guitar, "\nThis guitar is {} years old which makes this a vintage guitar".format(my_guitar.get_age()))
-    else:
-        print(my_guitar, "\nThis guitar is {} years old which doesn't make it a vintage.... yet.".format(my_guitar.
-                                                                                                         get_age()))
+    print("{} get_age() - Expected 97. Got {}".format(my_guitar.name, my_guitar.get_age()))
+    another_guitar = Guitar("Fender Stratocaster", 2014, 765.4)
+    print("{} get_age() - Expected 5. Got {}".format(another_guitar.name, another_guitar.get_age()))
+    print("{} is_vintage() - Expected True. Got {}".format(my_guitar.name, my_guitar.is_vintage()))
+    print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name, another_guitar.is_vintage()))
 
 
 main()
